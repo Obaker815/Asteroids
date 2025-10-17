f14443d5c80fd0906af37175abd2b5afe414c1a4
obaker815 <obaker815@gmail.com>
2025-10-17 12:42:09 +0100

    Add bounds and wrapping logic to Wrapable class
    Introduced `SetBounds(float w, float h)` to define screen bounds
    and `SetBounds(Size s)` as an overload for convenience. Added
    `WrapPosition()` to handle position wrapping within bounds.
    
    Updated the `Wrapable` constructor to enforce bounds
    initialization by throwing exceptions if `width` or `height`
    are unset.

--- Asteroids/Wrapable.cs
+++ Asteroids/Wrapable.cs
@@ -7,7 +7,22 @@ namespace Asteroids
         private static float width = 0;
         private static float height = 0;
 
+        /// <summary>
+        /// Overload for SetBounds(float, float) using Size
+        /// </summary>
+        /// <param name="s">Size of the screen</param>
+        public static void SetBounds(Size s) => SetBounds(s.Width, s.Height);
 
+        /// <summary>
+        /// Set the bounds of the screen for wrapping
+        /// </summary>
+        /// <param name="w">Width of the screen</param>
+        /// <param name="h">Height of the screen</param>
+        public static void SetBounds(float w, float h)
+        {
+            width = w;
+            height = h;
+        }
 
         /// <summary>
         /// Constructor for the Wrapable class
@@ -18,9 +33,14 @@ namespace Asteroids
             if (width == 0) throw new Exception("Wrapable width not set");
             if (height == 0) throw new Exception("Wrapable height not set");
         }
+
+        /// <summary>
+        /// Wrap the position of the object around the screen edges
+        /// </summary>
         public void WrapPosition()
         {
             Vector2 pos = base.position;
+            // vertical wrapping
             while (pos.X < 0)
             {
                 pos.X += width;
@@ -30,6 +50,7 @@ namespace Asteroids
                 pos.X -= width;
             }
 
+            // horizontal wrapping
             while (pos.Y < 0)
             {
                 pos.Y += height;
