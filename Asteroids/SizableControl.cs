0c10b646be1ae8d70a13adcd811cf84187bed115
obaker815 <obaker815@gmail.com>
2026-01-05 13:05:50 +0000

    removed scalableControl Class

--- Asteroids/SizableControl.cs
+++ Asteroids/SizableControl.cs
@@ -1,12 +1,12 @@
 ﻿namespace Asteroids
 {
-    internal class ScalableControl<T>(PointF position, SizeF size, float fontSize) where T : Control , new()
+    internal class SizableControl<T>(PointF position, SizeF size, float fontSize) where T : Control , new()
     {
         // private attributes
         private readonly T control = new();
         private PointF position    = position;
         private SizeF size         = size;
-        private float scale        = 1f;
+        private float scale        = 0f;
         private float fontSize     = fontSize;
 
         // public attribute lambdas
@@ -17,12 +17,12 @@
         public RectangleF Rectangle { get { return new(position, size); } }
         public T Control            { get { return control; } }
 
-        public void Update(float scale = 0)
+        public void Update(PointF AdditionalOffset, float scale = 1)
         {
-            if (scale != 0) this.scale = scale;
-            control.Location = new Point((int)(position.X * this.scale), (int)(position.Y  * this.scale));
+            this.scale = scale;
+            control.Location = new Point((int)(position.X * this.scale + AdditionalOffset.X), (int)(position.Y  * this.scale + AdditionalOffset.Y));
             control.Size     = new Size ((int)(size.Width * this.scale), (int)(size.Height * this.scale));
-            control.Font     = new Font(control.Font.FontFamily, fontSize * scale, control.Font.Style);
+            control.Font     = new Font(GameForm.PublicFonts?.Families[0]!, fontSize * scale, control.Font.Style);
         }
     }
 }
